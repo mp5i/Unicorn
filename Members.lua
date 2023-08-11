74187b5e9a6927d39439346f89fc302a7866a9c4
mp5i <141184721+mp5i@users.noreply.github.com>
2023-08-11 15:18:03 -0700

    Update Members.lua

--- Members.lua
+++ Members.lua
@@ -1,13 +1,13 @@
-loadstring(game:HttpGet("https://github.com/mp5i/Unicorn/blob/main/Engine.lua"))()
+local a = loadstring(game:HttpGet("https://github.com/mp5i/Unicorn/blob/main/Engine.lua"))()
 
 local Members = {}
 
 Members.GetRunService = function(Member)
     if (Member == "RenderStepped") then
-        return Engine.GetService("RunService").RenderStepped
+        return a.Engine.GetService("RunService").RenderStepped
     end
 end
 
 Members.GetRunService("RenderStepped"):Connect(function()
-            Engine.GetClass("StarterGui"):SetCore("SendNotification", {Title = "LOL", Text = "LOL", Duration = 3})
+            a.Engine.GetClass("StarterGui"):SetCore("SendNotification", {Title = "LOL", Text = "LOL", Duration = 3})
     end)
